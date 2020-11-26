8031e0e4abc1e2d8124ca28847acffed9e0367f0
allan sousa <allanportinfo@gmail.com>
2020-11-26 12:02:01 -0300

    estudo sobre os tipos aritmeticos

--- operadores.php
+++ operadores.php
@@ -1,9 +1,20 @@
 <?php
-// Operadores Númericos
+echo "Operadores Aritméticos";
 
+/*
++$a	Identidade - Conversão de $a para int ou float conforme apropriado.
+-$a	Negação - Oposto de $a.
+$a + $b	Adição - Soma de $a e $b.
+$a - $b	Subtração - Diferença entre $a e $b.
+$a * $b	Multiplicação - Produto de $a e $b.
+$a / $b	Divisão - Quociente de $a e $b.
+$a % $b	Módulo - Resto de $a dividido por $b.
+$a ** $b Exponencial - 	Resultado de $a elevado a $b. Introduzido no PHP 5.6.
+*/
+
+// Exemplo 1
 $idadeallan = 26;
-$idadeizabella = 26;
+$idadeizabella = 25;
 
 var_dump ($idadeallan == $idadeizabella);
 echo "<br>";
-
