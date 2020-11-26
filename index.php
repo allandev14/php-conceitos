5eed1a79d59fd2578e94d2b28df4537f4f797d60
allan sousa <allanportinfo@gmail.com>
2020-11-26 09:20:14 -0300

    salvando essa parte do codigo

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$nome = "Juliana";
+$idade = 23;
+$formacao = array();
+$ultimaVisualizacao = null;
+
+
+echo $nome ."<br>";
+echo $idade ."<br>";
+
+
+echo isset($formacao) ."<br>";
+echo isset($ultimaVisualizacacao);
+
+echo empty($formacao) ."<br>";
+echo empty($ultimaVisualzacao) ."<br>";
