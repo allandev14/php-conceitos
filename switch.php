e2f0ff2dbbbd83c7fe6d4399258cc09211fca14a
allan sousa <allanportinfo@gmail.com>
2020-11-26 16:19:55 -0300

    exemplo de for

--- switch.php
+++ switch.php
@@ -1,6 +1,6 @@
 <?php
 
-// Switch
+// Switch Case
 
 // A declaração switch é similar a uma série de declarações IF na mesma expressão.
 $corfavorita = "azul"; // azul, verde, amarelo.......
@@ -16,5 +16,15 @@ switch($corfavorita) {
     break;
     default: // seria igual ao else com condicionante negativa 
         echo "Não selecionou nehuma cor dispónivel";
+    }
+    echo "<br> <hr>";
+// Loop 
+// Setença de repetição dentro do PHP
 
+for($numero = 0; $numero <= 10; $numero++) { //nesse caso a separação será realizada com ; e não com ,
+    echo "O número atual é igual há : $numero <br>";
+}
+echo "<br> <hr>";
+for($i = 20; $i <= 25; $i++) {
+    echo "O número atual é igual há : $i <br>";
 }
