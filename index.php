89327c81c71acf0984b156796a2dd01ff801b5d2
allan sousa <allanportinfo@gmail.com>
2020-11-26 09:41:54 -0300

    tipos primitivos

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+// Variáveis mudam e acordo com o valor inserido no codigo 
 
 $nome = "Juliana";
 $idade = 23;
@@ -6,12 +7,30 @@ $formacao = array();
 $ultimaVisualizacao = null;
 
 
-echo $nome ."<br>";
-echo $idade ."<br>";
+echo $nome ."<br>"; // Juliana
+echo $idade ."<br>"; // 23
 
 
-echo isset($formacao) ."<br>";
-echo isset($ultimaVisualizacacao);
+echo isset($formacao) ."<br>"; //valor de saida verdadeiro ou true ou 1
+echo isset($ultimaVisualizacacao); //retrona o valor de false 
 
-echo empty($formacao) ."<br>";
-echo empty($ultimaVisualzacao) ."<br>";
+echo empty($formacao) ."<br>"; //valor de saida verdadeir ou true ou 1
+echo empty($ultimaVisualzacao) ."<br>"; //valor de saida verdadeir ou true ou 1
+
+// Constantes não variam dentro do código
+
+define('MEU_NOME', 'Allan sousa');
+    echo MEU_NOME;
+        echo "<br>";
+
+define('MINHA_IDADE', 26); // Exemplo 2
+        echo MINHA_IDADE;
+        echo "<br>";
+
+// Tipos primitivos São os tipos básicos usados dentro do PHP
+$nome01 = "Andressa". "<br>"; // string 
+$idade01 = "23". "<br>"; // int / integer
+$altura = "1.65". "<br>"; // float / double
+$mulher = true . "<br>"; // boolean
+$cursos = array('PHP', 'LARAVEL', 'MYSQL'). "<br>"; //array matriz de vetor dentro da sintaxe php
+$cursos01 = array['PHP', 'LARAVEL', 'MYSQL']. "<br>"; //Outra forma de inserir arrays dentro do código
