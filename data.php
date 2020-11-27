fcf916392a275868742b32becb03f3a49264fc99
allan sousa <allanportinfo@gmail.com>
2020-11-27 11:47:34 -0300

    estudo de horas

---
+++ data.php
@@ -0,0 +1,18 @@
+<?php
+
+// Date
+
+// d = dia
+// m = mês
+// Y = Ano
+
+echo "Hoje é dia " .date("d/m/Y"). "<br>";
+echo "Hoje é dia " .date("d.m.Y"). "<br>";
+echo "Hoje é dia " .date("Y-m-a"). "<br>";
+echo "Hoje é dia " .date("l"). "<br>";
+echo "<hr>";
+
+echo "Agora é " .date("h.i.s");
+echo "<hr>";
+
+echo "Agora a hora completa " .date("d.m.Y : h.i.s");
