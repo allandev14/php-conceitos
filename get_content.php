58801968910f7370252926fa40fa3d2a2ae7c3fe
allan sousa <allanportinfo@gmail.com>
2020-11-27 16:40:21 -0300

    comandos file_get_contents e file_put_contents

--- get_content.php
+++ get_content.php
@@ -2,5 +2,11 @@
 
 // Estudo de get_content
 
-$texto = file_get_contents('Ola.txt');
+$texto = file_get_contents('ola.txt'); //para importação de documentos externos ao codigo
 echo $texto;
+
+// Exemplo de escrita de um documento
+
+$escreva = file_put_contents('escreva.txt', 'Olá eu estou bem graças a Deus e você?');
+echo "{$escreva} <br>";
+
