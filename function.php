12f919f8b03538f4bf1a2b4cd6b498047e03f07f
allan sousa <allanportinfo@gmail.com>
2020-11-27 10:16:20 -0300

    exemplos com functions

---
+++ function.php
@@ -0,0 +1,18 @@
+<?php
+
+// Functions PHP
+
+function imprimir() {
+    echo "Olá, mundo!";
+}
+
+imprimir();
+echo "<br>";
+
+// Exemplo 2
+function escreva() {
+    echo "Aprendendo sobre funções";
+}
+
+escreva();
+echo "<br>";
