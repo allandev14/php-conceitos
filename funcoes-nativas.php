50e1f18ab74db0ad7e232077dcf4facc8fe1d3d9
allan sousa <allanportinfo@gmail.com>
2020-11-27 12:13:03 -0300

    estudos strtoupper / strtolower

--- funcoes-nativas.php
+++ funcoes-nativas.php
@@ -3,4 +3,12 @@
 
 // Funções Nativas PHP
 
-phpinfo();
+// phpinfo(); Comando para informações do ambiente php instalado
+
+$nome = "andre";
+echo strtoupper($nome); // strtoupper - Transforma a variável em caixa alta
+echo "<br>";
+
+$nome2 = "Ana";
+echo strtolower($nome2); // strtolower - Transforma a variável em letras minusculas
+echo "<br>";
