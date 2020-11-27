7d76c918bb643bab160e89e446ed213ad0b494e3
allan sousa <allanportinfo@gmail.com>
2020-11-27 13:01:52 -0300

    funcao substr

--- funcoes-nativas.php
+++ funcoes-nativas.php
@@ -24,3 +24,36 @@ echo "<br>";
 $nomenegrito = '<b>Nome:</b> Allan Sousa';
 echo htmlspecialchars($nomenegrito); // função de escape para o html dentro da variável
 echo "<br>";
+
+$pizzas = 'musarela, moda, napolitana'; //nesse caso o item de seperação é a , por isso ela está declarada na array
+$arrayDePizzas = explode(',','$pizzas'); //função de explosão dentro de uma array
+
+var_dump($arrayDePizzas);
+echo "<br>";
+
+$doces = ['bolo','chocolate','bala'];
+$stringDeDoces = implode(',', $doces); //função inversa basicamente ela transforma uma array em string também pode ser chamada com sinomimo de join
+
+echo $stringDeDoces;
+echo "<br>";
+
+/*
+$mensagem = "Olá meu nome é Allan";
+$nome3 = substr($mensagem, 5); //nesse caso ele irá retirar todo conteúdo antes da quinta posição contanto os espaços
+
+echo $nome3;
+echo "<br>";
+*/
+
+/*
+$mensagem = "Olá meu nome é Allan";
+$nome3 = substr($mensagem, -4); //nesse caso ele irá retirar todo conteúdo inicial deixando apenas os ultimos 4 caracteres da declarados
+
+echo $nome3;
+echo "<br>";
+*/
+$mensagem = "Olá meu nome é Allan";
+$nome3 = substr($mensagem,0, 2); //nesse caso ele irá retirar todo conteúdo inicial deixanod apenas os duas primeiras posições declaradas
+
+echo $nome3;
+echo "<br>";
