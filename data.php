51574506bf46f1f30e8524e9f3b8925ebd1e3e04
allan sousa <allanportinfo@gmail.com>
2020-11-27 11:59:13 -0300

    finalizando estudos com data e hora

--- data.php
+++ data.php
@@ -6,13 +6,15 @@
 // m = mês
 // Y = Ano
 
-echo "Hoje é dia " .date("d/m/Y"). "<br>";
-echo "Hoje é dia " .date("d.m.Y"). "<br>";
-echo "Hoje é dia " .date("Y-m-a"). "<br>";
-echo "Hoje é dia " .date("l"). "<br>";
+date_default_timezone_set('America/Sao_Paulo'); // Script para indicar o fuso-horário de hora atual
+
+echo "Hoje é dia " .date("d/m/Y"). "<br>"; //Conjuto para indicar data no formato em português
+echo "Hoje é dia " .date("d.m.Y"). "<br>"; // Outra forma de expor o data
+echo "Hoje é dia " .date("Y-m-a"). "<br>"; // Formato americano de apresentação de data
+echo "Hoje é dia " .date("l"). "<br>"; //Formato apresenta somente as horas
 echo "<hr>";
 
-echo "Agora é " .date("h.i.s");
+echo "Agora é " .date("h.i.s"); // Forma de se apresentar conjuto de data e hora
 echo "<hr>";
 
-echo "Agora a hora completa " .date("d.m.Y : h.i.s");
+echo "Agora a hora completa " .date("d.m.Y : h.i.s"); // Uma das mutias formas de apresentação de data e hora
